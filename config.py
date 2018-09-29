773e921caae4e63d2b6f42d3b2b4ed0c9f93ac8b
mschonfinkel <marcos.schonfinkel@gmail.com>
2018-09-29 17:40:58 -0300

    Removed flask_script files

--- config.py
+++ config.py
@@ -1,4 +1,17 @@
 import os.path
+
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-DEBUG = True
+class BaseConfig():
+    DEBUG = False
+    TESTING = False
+
+class DevelopmentConfig(BaseConfig):
+    DEBUG = True
+
+class TestingConfig(BaseConfig):
+    TESTING = True
+
+class ProductionConfig(BaseConfig):
+    pass
